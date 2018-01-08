c9ab5aa6f00e5a2fd7dd5f8dff1161d77fdc863e
lvfc <lv_fengchen@163.com>
2018-01-08 15:20:25 +0800

    调用index

--- main.go
+++ main.go
@@ -5,6 +5,23 @@ import (
 	_ "iHome_go/routers"
 )
 
+func ignoreStaticPath() {
+
+	//透明static
+	beego.InsertFilter("/", beego.BeforeRouter, TransparentStatic)
+	beego.InsertFilter("/*", beego.BeforeRouter, TransparentStatic)
+}
+
+func TransparentStatic(ctx *context.Context) {
+	orpath := ctx.Request.URL.Path
+	beego.Debug("request url: ", orpath)
+	//如果请求uri还有api字段,说明是指令应该取消静态资源路径重定向
+	if strings.Index(orpath, "api") >= 0 {
+		return
+	}
+	http.ServeFile(ctx.ResponseWriter, ctx.Request, "static/html/"+ctx.Request.URL.Path)
+}
 func main() {
+	ignoreStaticPath()
 	beego.Run()
 }
